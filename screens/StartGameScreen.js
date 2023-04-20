2f7c814cb319de6e8f7702ba4431dff218aacf6f
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2023-04-19 18:29:13 -0600

    first commit for new pratcie native project

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,13 @@
+import { TextInput, View, Button } from "react-native"
+
+const StartGameScreen = () => {
+  return (
+    <View>
+        <TextInput />
+        <Button title="Hello" />
+    </View>
+  )
+    
+}
+
+export default StartGameScreen
