70d8871069656894aef548eb7eac25bc1e004c35
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2023-06-07 21:15:57 -0600

    added more logic for guessing

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,11 +1,16 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Alert } from "react-native";
 
-import { useState} from "react"
+// * .bind() on functions lets you send predefined props/ variables into a funciton.
+
+import { useState } from "react";
 
 import Title from "../components/ui/Title";
 import Colors from "../constants/Colors";
 import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/ui/PirmaryButton";
 
+let minBoundry = 1;
+let maxBoundry = 100;
 function generateRandomBetween(min, max, exclude) {
   const rndNum = Math.floor(Math.random() * (max - min)) + min;
 
@@ -16,9 +21,35 @@ function generateRandomBetween(min, max, exclude) {
   }
 }
 
-const GameScreen = ({userNumber}) => {
-const initalGuess = generateRandomBetween(1, 100, userNumber)
-const [currentGuess, setCurrentGuess] = useState(initalGuess)
+
+const GameScreen = ({ userNumber }) => {
+  const initalGuess = generateRandomBetween(minBoundry, maxBoundry, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initalGuess);
+
+  let nextGuessHandler = (direction) => {
+    if (
+      (direction === "lower" && currentGuess < userNumber) ||
+      (direction === "greater" && currentGuess > userNumber)
+    ) {
+      Alert.alert("Don't lie", "Try again buddy", [
+        { text: "Sorry!", style: "cancel" },
+      ]);
+      return;
+    }
+    console.log(minBoundry, maxBoundry)
+
+    if (direction === "lower") {
+      maxBoundry = currentGuess;
+    } else {
+      minBoundry = currentGuess + 1;
+      const newRandomNum = generateRandomBetween(
+        minBoundry,
+        maxBoundry,
+        currentGuess
+      );
+      setCurrentGuess(newRandomNum);
+    }
+  };
 
   return (
     <View style={styles.screen}>
@@ -26,7 +57,14 @@ const [currentGuess, setCurrentGuess] = useState(initalGuess)
       <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or Lower</Text>
-        {/* + - */}
+        <View>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>
+            +
+          </PrimaryButton>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
+            -
+          </PrimaryButton>
+        </View>
       </View>
       {/* <View>LOG ROUNDS</View> */}
     </View>
@@ -37,7 +75,7 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 24,
-  }
+  },
 });
 
 export default GameScreen;
